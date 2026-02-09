9335307e2dd6ca1fba006909dfc34611ae36c4c5
Alberto Rolle <alberto.rolleortega@gmail.com>
2026-02-09 22:55:35 +0100

    chore: add Tailwind, shadcn/ui & storybook basic conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
       '@app': resolve(__dirname, './src/app'),
       '@features': resolve(__dirname, './src/features'),
-      '@shared': resolve(__dirname, './src/shared'),
+      '@ui': resolve(__dirname, './src/ui'),
     },
   },
 });
