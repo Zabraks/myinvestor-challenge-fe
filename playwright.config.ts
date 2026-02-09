675b883fa73c48b93a776e81d16302f432080ee5
Alberto Rolle <alberto.rolleortega@gmail.com>
2026-02-09 16:32:39 +0100

    chore: basic playwright conf

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  outputDir: './tests/test-results',
+  timeout: 30 * 1000,
+  retries: 0,
+  use: {
+    headless: true,
+    baseURL: 'http://localhost:5173',
+    viewport: { width: 1280, height: 720 },
+    trace: 'on-first-retry'
+  }
+})
