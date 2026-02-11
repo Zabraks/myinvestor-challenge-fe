ebcd3ff4592b39257538d630061d407f2e84e9de
Alberto Rolle <alberto.rolleortega@gmail.com>
2026-02-11 20:17:57 +0100

    feat(funds-list): added Funds list

---
+++ src/services/funds/getFunds.api.ts
@@ -0,0 +1,15 @@
+import { mapFundFromApi } from '@domain/funds/mappers';
+
+export const getFunds = async (page, limit, field, direction) => {
+  const sorting = field && direction ? `&sort=${field}%3A${direction}` : '';
+
+  const res = await fetch(`http://localhost:3000/funds?page=${page}&limit=${limit}${sorting}`);
+  if (!res.ok) throw new Error('Error fetching funds');
+
+  const { data, pagination } = await res.json();
+
+  return {
+    pagination,
+    data: data.map(mapFundFromApi),
+  };
+};
